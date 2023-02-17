5f141439b764db4f7ff5d5e3d719c1d60780c032
Georgiy <larsubivec@gmail.com>
2023-02-17 21:01:06 +0400

    first commit

---
+++ OOPPr1.3/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPPr1._3
+{
+    internal class Program
+    {
+        static void FindAverage(int a, int b, int c)
+        {
+            Console.WriteLine("Average: " + (((a+b+c)/3).ToString()));
+            Console.ReadKey();
+        }
+        static void Main(string[] args)
+        {
+            int a, b, c;
+            Console.WriteLine("Enter first argument");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter second argument");
+            b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter third argument");
+            c = Convert.ToInt32(Console.ReadLine());
+
+            FindAverage(a, b, c);
+        }
+    }
+}
